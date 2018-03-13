b068991a231b54793911ed6f8c48bc1aec9c55a6
bajajra30 <rahul.bajaj2016@vitstudent.ac.in>
2018-03-13 13:13:19 +0530

    correction 1

--- defs.h
+++ defs.h
@@ -121,7 +121,7 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int             chpr(void);
-int             cps(void);
+int             cps(int);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
