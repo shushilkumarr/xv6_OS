543e4ec758057fcf7723e64284c22c04847a1a2d
bajajra30 <rahul.bajaj2016@vitstudent.ac.in>
2018-03-13 13:19:36 +0530

    correction 3

--- defs.h
+++ defs.h
@@ -120,7 +120,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
-int             chpr(int);
+int             chpr(int,int);
 int             cps(void);
 
 // swtch.S
